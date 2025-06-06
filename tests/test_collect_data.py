34ffcf9e0ee4d36a4f7d77fd77667766ec301187
manaspathak11 <manaspathak11@gmail.com>
2025-06-06 02:39:11 -0500

    Clean: Move project files to repo root, remove Everything/fire nesting

---
+++ tests/test_collect_data.py
@@ -0,0 +1,7 @@
+from smokesentinel.scripts.collect_data import (
+    _download_firms,
+    _download_hrrr_smoke,
+    _download_openaq,
+    _download_purpleair
+)
+from smokesentinel.scripts.config import settings 
